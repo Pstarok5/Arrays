3ae2a066917843ae671be27b03476030698670db
Okoye Emmanuel <okoyepstar5@gmail.com>
2023-07-21 14:19:53 -0700

    fourth commit

---
+++ app.js
@@ -0,0 +1,55 @@
+// push array
+let names = ["Dad", "Mom", "Sister", "Uncle"];
+names.push("Nephew");
+console.log(names);
+
+
+// pop array
+let fruits = ["Mango", "Pawpaw", "Grape", "Apple"];
+fruits.pop();
+console.log(fruits);
+
+
+// unshift array
+let car = ["Lambo", "Ferrarri", "Maybach", "G-wagon"];
+car.unshift("Royce-rolls");
+console.log(car);
+
+
+// shift array
+let girls = ["Chidinma", "Ifunanya", "Rose", "Mary"];
+girls.shift();
+console.log(girls);
+
+
+// Join array
+let boys = ["Daniel", "Emmanuel", "Chibuike", "Habeeb"];
+console.log(boys.join(""));
+
+
+// splice array
+let watch = ["Ice-watch", "Rolex-watch", "Silver-watch", "Gold-watch"];
+watch.splice(2,0, "Rubber")
+console.log(watch);
+
+
+// slice array
+let foods = ["Beans", "Rice", "Indomie", "Garri"];
+console.log(foods.slice(0));
+
+
+
+// length array
+let numbers = ["100", "200", "300", "400"];
+console.log(numbers.length);
+
+
+
+// typeof array
+let alphabets = ["A", "B", "C", "D"];
+console.log(typeof "123");
+
+
+// Array.isArray array
+let filmz = ["Spider", "Merlin", "Jurassic", "Combat"];
+console.log(Array.isArray(filmz));
